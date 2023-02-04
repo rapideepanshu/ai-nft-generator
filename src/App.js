360251274754f6177aae56df48b61840c1cb6717
rapideepanshu <deepanshu@rapidinnovation.dev>
2023-02-04 21:11:39 +0530

    Setup hardhat and react and add NFT contract

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { NFTStorage, File } from "nft.storage";
+import { Buffer } from "buffer";
+import { ethers } from "ethers";
+import axios from "axios";
+
+// Components
+import Spinner from "react-bootstrap/Spinner";
+import Navigation from "./components/Navigation";
+
+// ABIs
+import NFT from "./abis/NFT.json";
+
+// Config
+import config from "./config.json";
+
+function App() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <div className="form">
+        <div className="image">
+          <img src="" alt="AI" />
+        </div>
+        <form>
+          <input type="text" placeholder="Create a name ....."></input>
+          <input type="text" placeholder="Create a description ..."></input>
+          <input type="submit" value="Generate & mint"></input>
+        </form>
+      </div>
+
+      <p>
+        View&nbsp;
+        <a href="" target="_blank">
+          Metadata
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
